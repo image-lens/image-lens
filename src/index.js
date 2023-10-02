1904bf03aff1838a8297a59b467902231c7ab860
kirillovmr <kirillovmr@gmail.com>
2023-10-02 11:29:15 +0600

    remove sandbox dist

--- src/index.js
+++ src/index.js
@@ -9,10 +9,6 @@ if (typeof window !== 'undefined') {
 		`%c✨ ImageLens v${ImageLens.version}`,
 		'color: black; background: white; font-size: 1.5em; padding: 0.25rem; margin: 0.5rem 0; border-radius: 4px; text-shadow: 1px 1px 0 #fffedd;'
 	)
-	console.log(
-		'%c✨  Images that are more useful.\n✨  https://github.com/kirillovmr/image-lens',
-		'color: black; background: white; padding: 0.25rem; margin: 0.5em 0; border-radius: 4px;'
-	)
 	console.log('')
 }
 
